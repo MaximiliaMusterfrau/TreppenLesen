dfaf9f9011118dff9195a7b9f7958ec49a0a23e7
MaximiliaMusterfrau <fam.lipp@web.de>
2026-07-28 08:12:22 +0200

    Refactor service worker for improved caching
    Updated caching strategy and added skipWaiting and claim methods.

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,19 @@
 const CACHE_NAME = "treppenlesen-v1";
 
-
 const FILES_TO_CACHE = [
-
-    "index.html",
-    "style.css",
-    "app.js",
-    "words.js",
-    "manifest.json"
-
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./words.js",
+    "./manifest.json"
 ];
-
-
-
-// Installation
 
 self.addEventListener("install", event => {
 
     event.waitUntil(
 
-        caches.open(CACHE_NAME)
-        .then(cache => {
+        caches.open(CACHE_NAME).then(cache => {
 
             return cache.addAll(FILES_TO_CACHE);
 
@@ -28,42 +21,15 @@ self.addEventListener("install", event => {
 
     );
 
-});
-
-
-
-
-// Dateien aus Cache laden
-
-self.addEventListener("fetch", event => {
-
-
-    event.respondWith(
-
-        caches.match(event.request)
-        .then(response => {
-
-            return response || fetch(event.request);
-
-        })
-
-    );
-
+    self.skipWaiting();
 
 });
-
-
-
-
-// Alte Versionen entfernen
 
 self.addEventListener("activate", event => {
 
-
     event.waitUntil(
 
-        caches.keys()
-        .then(keys => {
+        caches.keys().then(keys => {
 
             return Promise.all(
 
@@ -83,5 +49,20 @@ self.addEventListener("activate", event => {
 
     );
 
+    self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request).then(response => {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
 
 });
