e0207e0dde886c08975ab186b72fb41ec5f63295
MaximiliaMusterfrau <fam.lipp@web.de>
2026-07-28 10:02:15 +0200

    Update words.js

--- words.js
+++ words.js
@@ -1,148 +1,84 @@
+// ==============================
+// TreppenLesen
+// words.js
+// Teil 1 von 3
+// ==============================
+
 const words = [
 
+    // ==========================
     // LW1
-    { word: "Name", list: "LW1" },
-    { word: "lesen", list: "LW1" },
-    { word: "Ei", list: "LW1" },
-    { word: "Eier", list: "LW1" },
-    { word: "ein", list: "LW1" },
-    { word: "eine", list: "LW1" },
+    // ==========================
 
+    { word:"Name", list:"LW1" },
+    { word:"lesen", list:"LW1" },
+    { word:"Ei", list:"LW1" },
+    { word:"Eier", list:"LW1" },
+    { word:"ein", list:"LW1" },
+    { word:"eine", list:"LW1" },
+
+    // ==========================
     // LW2
-    { word: "unter", list: "LW2" },
-    { word: "bunt", list: "LW2" },
-    { word: "Bein", list: "LW2" },
-    { word: "aber", list: "LW2" },
+    // ==========================
 
+    { word:"unter", list:"LW2" },
+    { word:"bunt", list:"LW2" },
+    { word:"Bein", list:"LW2" },
+    { word:"aber", list:"LW2" },
+
+    // ==========================
     // LW3
-    { word: "Buch", list: "LW3" },
-    { word: "suchen", list: "LW3" },
-    { word: "ich", list: "LW3" },
-    { word: "mit", list: "LW3" },
+    // ==========================
 
+    { word:"Buch", list:"LW3" },
+    { word:"suchen", list:"LW3" },
+    { word:"ich", list:"LW3" },
+    { word:"mit", list:"LW3" },
+
+    // ==========================
     // LW4
-    { word: "Foto", list: "LW4" },
-    { word: "rufen", list: "LW4" },
-    { word: "fein", list: "LW4" },
-    { word: "Klasse", list: "LW4" },
-    { word: "kommen", list: "LW4" },
+    // ==========================
 
+    { word:"Foto", list:"LW4" },
+    { word:"rufen", list:"LW4" },
+    { word:"fein", list:"LW4" },
+    { word:"Klasse", list:"LW4" },
+    { word:"kommen", list:"LW4" },
+
+    // ==========================
     // LW5
-    { word: "Schule", list: "LW5" },
-    { word: "schreiben", list: "LW5" },
-    { word: "er schreibt", list: "LW5" },
-    { word: "Traum", list: "LW5" },
-    { word: "Träume", list: "LW5" },
-    { word: "Auto", list: "LW5" },
-    { word: "laufen", list: "LW5" },
-    { word: "er läuft", list: "LW5" },
+    // ==========================
 
+    { word:"Schule", list:"LW5" },
+    { word:"schreiben", list:"LW5" },
+    { word:"schreibt", list:"LW5" },
+    { word:"Traum", list:"LW5" },
+    { word:"Träume", list:"LW5" },
+    { word:"Auto", list:"LW5" },
+    { word:"laufen", list:"LW5" },
+    { word:"läuft", list:"LW5" },
+
+    // ==========================
     // LW6
-    { word: "Wort", list: "LW6" },
-    { word: "wünschen", list: "LW6" },
-    { word: "er wünscht", list: "LW6" },
-    { word: "wollen", list: "LW6" },
-    { word: "er will", list: "LW6" },
-    { word: "üben", list: "LW6" },
-    { word: "er übt", list: "LW6" },
-    { word: "müssen", list: "LW6" },
-    { word: "er muss", list: "LW6" },
+    // ==========================
 
+    { word:"Wort", list:"LW6" },
+    { word:"wünschen", list:"LW6" },
+    { word:"wünscht", list:"LW6" },
+    { word:"wollen", list:"LW6" },
+    { word:"will", list:"LW6" },
+    { word:"üben", list:"LW6" },
+    { word:"übt", list:"LW6" },
+    { word:"müssen", list:"LW6" },
+    { word:"muss", list:"LW6" },
+
+    // ==========================
     // LW7
-    { word: "Erde", list: "LW7" },
-    { word: "dunkel", list: "LW7" },
-    { word: "Juli", list: "LW7" },
-    { word: "jeder", list: "LW7" },
-    { word: "jede", list: "LW7" },
-    { word: "jedes", list: "LW7" },
+    // ==========================
 
-    // LW8
-    { word: "Puppe", list: "LW8" },
-    { word: "Ampel", list: "LW8" },
-    { word: "Gemüse", list: "LW8" },
-    { word: "geben", list: "LW8" },
-    { word: "er gibt", list: "LW8" },
-
-    // LW9
-    { word: "Körper", list: "LW9" },
-    { word: "können", list: "LW9" },
-    { word: "er kann", list: "LW9" },
-    { word: "Teddy", list: "LW9" },
-    { word: "Teddys", list: "LW9" },
-    { word: "Baby", list: "LW9" },
-    { word: "Babys", list: "LW9" },
-
-    // LW10
-    { word: "Freund", list: "LW10" },
-    { word: "Freunde", list: "LW10" },
-    { word: "neu", list: "LW10" },
-    { word: "neun", list: "LW10" },
-    { word: "Fuß", list: "LW10" },
-    { word: "Füße", list: "LW10" },
-
-    // LW11
-    { word: "Wiese", list: "LW11" },
-    { word: "Tier", list: "LW11" },
-    { word: "sie", list: "LW11" },
-    { word: "Pferd", list: "LW11" },
-    { word: "Pferde", list: "LW11" },
-    { word: "pflegen", list: "LW11" },
-    { word: "er pflegt", list: "LW11" },
-    { word: "Hund", list: "LW11" },
-    { word: "Hunde", list: "LW11" },
-    { word: "haben", list: "LW11" },
-    { word: "er hat", list: "LW11" },
-
-    // LW12
-    { word: "Finger", list: "LW12" },
-    { word: "fangen", list: "LW12" },
-    { word: "er fängt", list: "LW12" },
-    { word: "bringen", list: "LW12" },
-    { word: "er bringt", list: "LW12" },
-    { word: "Zahn", list: "LW12" },
-    { word: "Zähne", list: "LW12" },
-    { word: "zeigen", list: "LW12" },
-    { word: "er zeigt", list: "LW12" },
-
-    // LW13
-    { word: "Zucker", list: "LW13" },
-    { word: "Rücken", list: "LW13" },
-    { word: "Rock", list: "LW13" },
-    { word: "Röcke", list: "LW13" },
-    { word: "Sport", list: "LW13" },
-    { word: "spielen", list: "LW13" },
-    { word: "er spielt", list: "LW13" },
-
-    // LW14
-    { word: "Stange", list: "LW14" },
-    { word: "Stift", list: "LW14" },
-    { word: "stellen", list: "LW14" },
-
-    // LW15
-    { word: "Ball", list: "LW15" },
-    { word: "Bälle", list: "LW15" },
-    { word: "Mädchen", list: "LW15" },
-    { word: "zählen", list: "LW15" },
-    { word: "Hexe", list: "LW15" },
-    { word: "Lexikon", list: "LW15" },
-    { word: "Lexika", list: "LW15" },
-
-    // LW16
-    { word: "Quadrat", list: "LW16" },
-    { word: "quaken", list: "LW16" },
-    { word: "Vogel", list: "LW16" },
-    { word: "Vögel", list: "LW16" },
-    { word: "Vater", list: "LW16" },
-    { word: "versuchen", list: "LW16" },
-    { word: "er versucht", list: "LW16" },
-
-    // LW17
-    { word: "Computer", list: "LW17" },
-    { word: "Cent", list: "LW17" },
-    { word: "Cents", list: "LW17" },
-    { word: "Katze", list: "LW17" },
-    { word: "Platz", list: "LW17" },
-    { word: "Plätze", list: "LW17" },
-    { word: "sitzen", list: "LW17" }
-
-];
+    { word:"Erde", list:"LW7" },
+    { word:"dunkel", list:"LW7" },
+    { word:"Juli", list:"LW7" },
+    { word:"jeder", list:"LW7" },
+    { word:"jede", list:"LW7" },
+    { word:"jedes", list:"LW7" },
