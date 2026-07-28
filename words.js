479141866452c4432f50c47df963d5f8a1822249
MaximiliaMusterfrau <fam.lipp@web.de>
2026-07-28 10:05:46 +0200

    Add new words to words.js for LW8 to LW17

--- words.js
+++ words.js
@@ -82,3 +82,122 @@ const words = [
     { word:"jeder", list:"LW7" },
     { word:"jede", list:"LW7" },
     { word:"jedes", list:"LW7" },
+        // ==========================
+    // LW8
+    // ==========================
+
+    { word:"Puppe", list:"LW8" },
+    { word:"Ampel", list:"LW8" },
+    { word:"Gemüse", list:"LW8" },
+    { word:"geben", list:"LW8" },
+    { word:"gibt", list:"LW8" },
+
+    // ==========================
+    // LW9
+    // ==========================
+
+    { word:"Körper", list:"LW9" },
+    { word:"können", list:"LW9" },
+    { word:"kann", list:"LW9" },
+    { word:"Teddy", list:"LW9" },
+    { word:"Teddys", list:"LW9" },
+    { word:"Baby", list:"LW9" },
+    { word:"Babys", list:"LW9" },
+
+    // ==========================
+    // LW10
+    // ==========================
+
+    { word:"Freund", list:"LW10" },
+    { word:"Freunde", list:"LW10" },
+    { word:"neu", list:"LW10" },
+    { word:"neun", list:"LW10" },
+    { word:"Fuß", list:"LW10" },
+    { word:"Füße", list:"LW10" },
+
+    // ==========================
+    // LW11
+    // ==========================
+
+    { word:"Wiese", list:"LW11" },
+    { word:"Tier", list:"LW11" },
+    { word:"sie", list:"LW11" },
+    { word:"Pferd", list:"LW11" },
+    { word:"Pferde", list:"LW11" },
+    { word:"pflegen", list:"LW11" },
+    { word:"pflegt", list:"LW11" },
+    { word:"Hund", list:"LW11" },
+    { word:"Hunde", list:"LW11" },
+    { word:"haben", list:"LW11" },
+    { word:"hat", list:"LW11" },
+
+    // ==========================
+    // LW12
+    // ==========================
+
+    { word:"Finger", list:"LW12" },
+    { word:"fangen", list:"LW12" },
+    { word:"fängt", list:"LW12" },
+    { word:"bringen", list:"LW12" },
+    { word:"bringt", list:"LW12" },
+    { word:"Zahn", list:"LW12" },
+    { word:"Zähne", list:"LW12" },
+    { word:"zeigen", list:"LW12" },
+    { word:"zeigt", list:"LW12" },
+
+    // ==========================
+    // LW13
+    // ==========================
+
+    { word:"Zucker", list:"LW13" },
+    { word:"Rücken", list:"LW13" },
+    { word:"Rock", list:"LW13" },
+    { word:"Röcke", list:"LW13" },
+    { word:"Sport", list:"LW13" },
+    { word:"spielen", list:"LW13" },
+    { word:"spielt", list:"LW13" },
+        // ==========================
+    // LW14
+    // ==========================
+
+    { word:"Stange", list:"LW14" },
+    { word:"Stift", list:"LW14" },
+    { word:"stellen", list:"LW14" },
+
+    // ==========================
+    // LW15
+    // ==========================
+
+    { word:"Ball", list:"LW15" },
+    { word:"Bälle", list:"LW15" },
+    { word:"Mädchen", list:"LW15" },
+    { word:"zählen", list:"LW15" },
+    { word:"Hexe", list:"LW15" },
+    { word:"Lexikon", list:"LW15" },
+    { word:"Lexika", list:"LW15" },
+
+    // ==========================
+    // LW16
+    // ==========================
+
+    { word:"Quadrat", list:"LW16" },
+    { word:"quaken", list:"LW16" },
+    { word:"Vogel", list:"LW16" },
+    { word:"Vögel", list:"LW16" },
+    { word:"Vater", list:"LW16" },
+    { word:"versuchen", list:"LW16" },
+    { word:"versucht", list:"LW16" },
+
+    // ==========================
+    // LW17
+    // ==========================
+
+    { word:"Computer", list:"LW17" },
+    { word:"Cent", list:"LW17" },
+    { word:"Cents", list:"LW17" },
+    { word:"Katze", list:"LW17" },
+    { word:"Platz", list:"LW17" },
+    { word:"Plätze", list:"LW17" },
+    { word:"sitzen", list:"LW17" }
+
+];
